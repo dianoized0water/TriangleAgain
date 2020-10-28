0ca414dbd142a5e555dca0449e3ade85b33250d1
dianoized0water <62660947+dianoized0water@users.noreply.github.com>
2020-10-28 19:10:29 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -8,7 +8,7 @@ public class Triangle{
   }
 
   public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
-    v1 = new Point(x1, x2);
+    v1 = new Point(x1, y1);
     v2 = new Point(x2, y2);
     v3 = new Point(x3, y3);
   }
@@ -27,9 +27,9 @@ public class Triangle{
   }
 
   public String classify(){
-    double side1 = (Math.round(1000.0*v1.distanceTo(v2))/ 1000.0);
-    double side2 = (Math.round(1000.0*v2.distanceTo(v3))/ 1000.0);
-    double side3 = (Math.round(1000.0*v3.distanceTo(v1))/ 1000.0);
+    double side1 = (Math.round(10000.0*v1.distanceTo(v2))/ 10000.0);
+    double side2 = (Math.round(10000.0*v2.distanceTo(v3))/ 10000.0);
+    double side3 = (Math.round(10000.0*v3.distanceTo(v1))/ 10000.0);
     if (side1==side2){
       if (side2==side3){
         return "equilateral";
@@ -50,5 +50,17 @@ public class Triangle{
 
   public String toString(){
     return "v1(" + v1.getX() + ", " + v1.getY() + ") v2(" + v2.getX() + ", " + v2.getY() + ") v3(" + v3.getX()+ ", " + v3.getY() + ")";
+  }
+
+  public void setVertex(int index, Point newP){
+    if (index==0){
+      v1 = newP;
+    }
+    if (index==1){
+      v2 = newP;
+    }
+    if (index==2){
+      v3 = newP;
+    }
   }
 }
