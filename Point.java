e82044cb8271a4898130372cb1c27c45fddbbfd0
diana_yang <dyang20@stuy.edu>
2020-10-28 16:54:08 -0400

    beginning of the class

---
+++ Point.java
@@ -0,0 +1,27 @@
+public class Point{
+  private double x,y;
+
+  public Point(double X, double Y){
+    x=X;
+    y=Y;
+  }
+
+  public Point(Point p){
+    x=p.x;
+    y=p.y;
+  }
+
+  public double getX(){
+    return x;
+  }
+
+  public double getY(){
+    return y;
+  }
+
+  public double distanceTo(Point other){
+    double deltaX = ((this.x - other.x)*(this.x - other.x));
+    double deltaY = ((this.y - other.y)*(this.y - other.y));
+    return 
+  }
+}
